f526baf3a91291e053895eef012853b719761456
aniket <ani1998ket@gmail.com>
2020-02-02 10:32:09 +0530

    Added animation for tower

--- draw.lua
+++ draw.lua
@@ -5,14 +5,15 @@ function drawObjects()
     for i = 1, ENEMY_COUNT do 
         drawImage(enemies[i],zombieSprites[0])    
     end
-    drawCircle(player)
     drawImage(player,playerSprites[0])
-    -- love.graphics.setColor(0, 0, 1)
-    -- drawCircle(tower)
-    -- love.graphics.setColor(1, 0, 0)
-    -- drawCircle(player)
-    -- love.graphics.setColor(1, 1, 1)
+    local frame = math.floor(towerMeter.currentFrame)
+    local meter = towerMeter.currentMeter
+    drawImage(towerMeter, towerMeters[meter][frame]);
     love.graphics.arc( "fill", player.x, player.y, player.size * WEAPON_SIZE_FACTOR  ,view_arc_angle,view_arc_angle + math.pi/6 )
+
+
+    love.graphics.print(tower.health, 100)
+    love.graphics.print(score, 600)
 end
 
 
--- updateObjects.lua
+++ updateObjects.lua
@@ -2,20 +2,44 @@ require "utility"
 require "config"
 
 function updateObjects(dt)
+    updateTower(dt)
+
     view_arc_angle = view_arc_angle + WEAPON_SWING_SPEED*dt
-    updateObject(player, dt)
+    
     updateObject(tower, dt)
+    -- zombie tower collision
     for i = 1, ENEMY_COUNT do
         updateObject(enemies[i], dt)
         if (distance(enemies[i].x, enemies[i].y, tower.x, tower.y) < tower.size + enemies[i].size) then
             enemies[i].vx = 0
             enemies[i].vy = 0
+            tower.health = tower.health - TOWER_DAMAGE_SPEED * dt;
         end
         if(distance(enemies[i].x, enemies[i].y, player.x, player.y) < WEAPON_SIZE_FACTOR *player.size) then
             removeEnemy(enemies[i]);
+            score = score + 1
+            TOWER_DAMAGE_SPEED = TOWER_DAMAGE_SPEED + 0.1
         end
     end
 
+    -- repair tower
+    updateObject(player, dt)
+    if (distance(player.x, player.y, tower.x, tower.y) < tower.size + player.size) then
+        local length = distance(player.x, player.y, tower.x, tower.y)
+        local x = (player.x - tower.x) / length
+        local y = (player.y - tower.y) / length
+        player.x = x * (tower.size + player.size) + tower.x
+        player.y = y * (tower.size + player.size) + tower.y
+        tower.health = tower.health + TOWER_REPAIR_SPEED * dt;
+    end
+    if(tower.health < 0) then 
+        tower.health = 0
+        love.graphics.print("Game Over", 100, 200)
+    end
+    if(tower.health > 100) then 
+        tower.health = 100
+    end
+    
     -- bounding player to screen
     boundPlayerToScreen(player)
 end
@@ -45,5 +69,17 @@ function boundPlayerToScreen(player)
     end
     if (player.y > WINDOW_HEIGHT-player.size) then
     player.y =WINDOW_HEIGHT-player.size
+    end
+end
+
+
+function updateTower(dt) 
+    towerMeter.currentFrame = (towerMeter.currentFrame + 10*dt) % 5
+    towerMeter.currentMeter = 2
+    if(tower.health > 20) then
+        towerMeter.currentMeter = 1 
+    end
+    if(tower.health > 50) then
+        towerMeter.currentMeter = 0
     end
 end
